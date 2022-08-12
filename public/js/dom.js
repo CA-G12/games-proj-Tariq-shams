bc12ff3d1dd5331804e02797165598b067fb81e9
shamskhodary <shams.khodary@gmail.com>
2022-08-12 20:14:28 +0300

    Made the functionality for the input & submit button Relates #15

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,5 +1,50 @@
-const input = document.querySelector('#search');
-const dataLists = document.querySelector('#gameLists');
-const submit = document.querySelector('.search');
-const container = document.querySelector('.container');
+const input = document.querySelector("#search");
+const dataLists = document.querySelector("#gameLists");
+const submit = document.querySelector(".search");
+const container = document.querySelector(".container");
 let state = [];
+
+function addCard(state) {
+  const { value } = input;
+  for (let i = 0; i < state.length; i++) {
+    if (state[i]["title"].toLowerCase().includes(value.toLowerCase())) {
+      let divContainer = document.createElement("div");
+      divContainer.classList = "card";
+      container.appendChild(divContainer);
+      let img = document.createElement("img");
+      img.src = state[i]["thumbnail"];
+      img.alt = state[i]["title"];
+      divContainer.appendChild(img);
+      let title = document.createElement("h3");
+      title.textContent = state[i]["title"];
+      divContainer.appendChild(title);
+    }
+  }
+}
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  container.textContent = "";
+  const { value } = input;
+  if (value) {
+    fetch(`../api.json`, (res) => {
+      addCard(res);
+    });
+  } else {
+    return;
+  }
+});
+
+function renderSuggestions(state) {
+  dataLists.textContent = "";
+
+  state.forEach((ele) => {
+    const options = document.createElement("option");
+    options.value = ele["name"];
+    dataLists.appendChild(options);
+  });
+}
+
+input.addEventListener("input", () => {
+  fetch(`../data.json`, (res) => renderSuggestions(res));
+});
