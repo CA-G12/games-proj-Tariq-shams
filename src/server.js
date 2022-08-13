cb283e7bcab56b71079cfded425e31a3c9ad7e34
shamskhodary <shams.khodary@gmail.com>
2022-08-13 11:42:42 +0300

    changed the port to 3003 Relates #22

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const router = require('./router');
 
-const port = 2022;
+const port = 3003;
 
 http.createServer(router).listen(port, () => {
   // eslint-disable-next-line no-console
